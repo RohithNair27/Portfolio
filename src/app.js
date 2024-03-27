683c9c3d579e3ecb9db933a3739f60c69b17559b
rohith <rohithnair2711@gmail.com>
2024-03-28 01:02:32 +0530

    Enhancement: Introduction component made responsive

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
+// onScroll transform
 const observer = new IntersectionObserver(
   function (entries) {
     entries.forEach((entry) => {
-      console.log(entry);
       entry.target.classList.toggle("show", entry.isIntersecting);
       if (entry.isIntersecting) {
         observer.unobserve(entry.target);
@@ -13,17 +13,57 @@ const observer = new IntersectionObserver(
   }
 );
 const classes = [
-  ".contact-button1",
-  ".contact-button2",
-  ".contact-button3",
+  "#contact-button1",
+  "#contact-button2",
+  "#contact-button3",
   ".skill.one",
   ".skill.two",
   ".circle1",
   ".techstack",
 ];
 classes.forEach((className) => {
-  const elements = document.querySelectorAll(className);
-  elements.forEach((element) => {
-    observer.observe(element);
-  });
+  if (className.startsWith("#")) {
+    let elements = document.getElementById(className.slice(1));
+    observer.observe(elements);
+  } else {
+    let elements = document.querySelectorAll(className);
+    elements.forEach((element) => {
+      observer.observe(element);
+    });
+  }
 });
+
+// Scroll to a specific element
+function scrollToElement(targetElementId) {
+  const targetElement = document.getElementById(targetElementId);
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+// const sections = [
+//   "skillwrapper",
+//   "projectswrapper",
+//   "experiences-wraper",
+//   "footer",
+// ];
+
+// document.getElementById("navigateBottom").addEventListener("click", () => {
+//   if (currentIndex <= sections.length - 1) {
+//     scrollToElement(sections[currentIndex]);
+//     currentIndex++;
+//   }
+// });
+
+document.getElementById("contact-button1").addEventListener("click", () => {
+  scrollToElement("skillwrapper");
+});
+document
+  .getElementById("contact-button2")
+  .addEventListener("click", () => scrollToElement("projectswrapper"));
+document
+  .getElementById("contact-button3")
+  .addEventListener("click", () => scrollToElement("experiences-wraper"));
+document
+  .getElementById("navigateTop")
+  .addEventListener("click", () => scrollToElement("introduction"));
