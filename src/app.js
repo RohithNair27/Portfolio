666a0dfba5d21cf3397d03612d38e3e60f7d0c77
rohithnair2711 <rohithnair2711@gmail.com>
2024-10-17 09:54:51 +0000

    feat:Changed the whole UI and added skills

--- src/app.js
+++ src/app.js
@@ -1,3 +1,63 @@
+//Keeping header consistant in all the pages using JS
+// const headerComponent = document.querySelector('header')
+// headerComponent.innerHTML=`<div>
+// <h1><a href=".">RK</a></h1>
+// <button>mode</button></div>`
+
+
+const allTechnologies = [
+  { name: "Vue JS", image: "./assets/vue.svg", category: 'frontend' },
+  { name: "React JS", image: "./assets/react.svg", category: 'frontend' },
+  { name: "Vite", image: "./assets/vite.svg", category: 'frontend' },
+  { name: "Expo", image: "./assets/expo.svg", category: 'frontend' },
+  { name: "Javascript", image: "./assets/javascript.svg", category: 'frontend' },
+  { name: "Typescript", image: "./assets/typescript.svg", category: 'frontend' },
+  
+  { name: "Node js", image: "./assets/node.svg", category: 'backend' },
+  { name: "Express", image: "./assets/express.svg", category: 'backend' },
+  
+  { name: "Git", image: "./assets/git.svg", category: 'tools' },
+  { name: "Github", image: "./assets/github.svg", category: 'tools' },
+  { name: "Firebase", image: "./assets/firebase.svg", category: 'tools' },
+  { name: "VS Code", image: "./assets/vs-code.svg", category: 'tools' },
+  { name: "Android Studio", image: "./assets/android-studio.svg", category: 'tools' },
+];
+
+const FrontendComponent = document.querySelector('.tech-stack-list-one');
+const BackendComponent = document.querySelector('.tech-stack-list-two');
+const ToolsComponent = document.querySelector('.tech-stack-list-three');
+
+let frontendHtmlContent = '';
+let backendHtmlContent = '';
+let toolsHtmlContent = '';
+
+allTechnologies.forEach((element) => {
+  // Create the HTML content for each technology
+  const htmlContent = `
+    <li>
+      <div class="each-tech-stack-container ${element.category}">
+        <img src="${element.image}"/>
+        <span>${element.name}</span>
+      </div>
+    </li>`;
+
+  // Append to the respective category
+  if (element.category === 'frontend') {
+    frontendHtmlContent += htmlContent;
+  } else if (element.category === 'backend') {
+    backendHtmlContent += htmlContent;
+  } else if (element.category === 'tools') {
+    toolsHtmlContent += htmlContent;
+  }
+});
+
+// Set the inner HTML for each component
+FrontendComponent.innerHTML = frontendHtmlContent;
+BackendComponent.innerHTML = backendHtmlContent;
+ToolsComponent.innerHTML = toolsHtmlContent;
+
+
+
 // onScroll transform
 const observer = new IntersectionObserver(
   function (entries) {
